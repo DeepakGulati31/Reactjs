3385613ede92a3272729b830d78c3749d75e8378
Deepak gulati <deepakgulati@Deepaks-MacBook-Air.local>
2024-04-23 15:29:53 +0530

    reactRouter

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home/Home.jsx'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data=useLoaderData()
+    // const [data,setData]=useState([]);
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/DeepakGulati31')
+    //     .then(res=>res.json())
+    //     .then(data=>{
+            
+    //         setData(data)
+    //     })
+    // },)
+
+    return (
+        <div className='text-center m-5 bg-gray-700 text-white p-4 text-3xl'>Github Followers: {data.followers}
+        <img src={data.avatar_url} alt="Git picture" width={300} />
+        </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader=async()=>{
+   const response= await fetch("https://api.github.com/users/DeepakGulati31")
+   return response.json()
+}
