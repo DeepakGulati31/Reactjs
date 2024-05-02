55189a559f198eeed642223f64acdcf9d1083a07
DeepakGulati31 <gulatideepak844@gmail.com>
2024-05-03 00:03:06 +0530

    Components added

--- 12megaBlog/src/App.jsx
+++ 12megaBlog/src/App.jsx
@@ -1,14 +1,51 @@
-import './App.css'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import {Outlet} from "react-router-dom"
 
 function App() {
-// console.log(process.env.REACT_APP_APPWRITE_URL)  //jyada tr process.env krtee h or ye create react app me hta vite ka alg h
-console.log(import.meta.env.VITE_APPWRITE_URL) 
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className=" w-full block">
+        <Header />
+        <main>
+          TODO:<Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 
   return (
     <>
-    <h1>Blog App With Appwrite</h1>
+      <h1>Blog App With Appwrite</h1>
     </>
-  )
+  );
 }
 
 export default App;
+
+// console.log(process.env.REACT_APP_APPWRITE_URL)  //jyada tr process.env krtee h or ye create react app me hta vite ka alg h
+// console.log(import.meta.env.VITE_APPWRITE_URL)
